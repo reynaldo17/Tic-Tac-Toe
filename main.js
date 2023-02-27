b48a5f86a4216b4c67b80917215f54a3da9a50a2
reynaldo17 <65983137+reynaldo17@users.noreply.github.com>
2023-02-27 14:59:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
+
+function render() {
+    board.forEach(function(mark, index) {
+\    squares[index].textContent = mark;
+    });
+};
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+ };
