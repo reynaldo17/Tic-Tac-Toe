b07d1769f3f3e6b991c499d6eb28789475f0446b
reynaldo17 <65983137+reynaldo17@users.noreply.github.com>
2023-02-27 15:09:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+    
 
 /*----- app's state (variables) -----*/
 let board;
@@ -18,6 +29,8 @@ const messages = document.querySelector('h2');
 
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
+document.getElementById('board').addEventListener('click', handleMove);
+document.getElementById('reset-button').addEventListener('click', init);
 
 /*----- functions -----*/
 function handleTurn(event) {
@@ -31,14 +44,15 @@ function handleTurn(event) {
     return square === event.target;
     });
     board[idx] = turn;
-    // This is tidy
     turn = turn === 'X' ? 'O' : 'X';
-    // In an if statement it would look like: 
-    // if (turn === 'X') {
-    // turn = 'O' 
-    // } else {
-    // turn = 'X' 
-    // };
-    // writing the ternary saved you from all that. 
+    win = getWinner();
     render();
+};
+
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+      });
+      return winner ? winner : board.includes('') ? null : 'T';
 };
